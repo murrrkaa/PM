ba6d955479c9e6c4471ebcaeef2ce86cd36e91e8
Maria Babina <mashababina857@gmail.com>
2025-01-07 14:44:55 +0300

    changed icon size

--- PM/src/shared/ui/icons/minus.tsx
+++ PM/src/shared/ui/icons/minus.tsx
@@ -8,8 +8,8 @@ export const IconMinus: FC<IIconProps> = () => {
       x="0px"
       y="0px"
       viewBox="0 0 485 485"
-      width="20"
-      height="20"
+      width="16"
+      height="16"
     >
       <rect y="227.5" width="485" height="30" />
     </svg>

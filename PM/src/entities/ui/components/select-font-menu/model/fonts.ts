803f7d9c8530e11b40772018d9ba2ae6b450214a
Maria Babina <mashababina857@gmail.com>
2025-01-07 15:21:32 +0300

    added font menu
    added fonts list

---
+++ PM/src/entities/ui/components/select-font-menu/model/fonts.ts
@@ -0,0 +1,39 @@
+interface IFont {
+  name: string;
+  value: string;
+}
+
+export const fonts: IFont[] = [
+  { name: "Times New Roman", value: "Times New Roman, serif" },
+  { name: "Georgia", value: "Georgia, serif" },
+  { name: "Garamond", value: "Garamond, serif" },
+  { name: "Palatino Linotype", value: "Palatino Linotype, serif" },
+  { name: "Book Antiqua", value: "Book Antiqua, serif" },
+  { name: "Arial", value: "Arial, sans-serif" },
+  { name: "Verdana", value: "Verdana, sans-serif" },
+  { name: "Helvetica", value: "Helvetica, sans-serif" },
+  { name: "Tahoma", value: "Tahoma, sans-serif" },
+  { name: "Trebuchet MS", value: "Trebuchet MS, sans-serif" },
+  { name: "Gill Sans", value: "Gill Sans, sans-serif" },
+  { name: "Segoe UI", value: "Segoe UI, sans-serif" },
+  {
+    name: "Franklin Gothic Medium",
+    value: "Franklin Gothic Medium, sans-serif",
+  },
+  { name: "Courier New", value: "Courier New, monospace" },
+  { name: "Lucida Console", value: "Lucida Console, monospace" },
+  { name: "Consolas", value: "Consolas, monospace" },
+  { name: "Monaco", value: "Monaco, monospace" },
+  { name: "Roboto", value: "Roboto, sans-serif" },
+  { name: "Ubuntu", value: "Ubuntu, sans-serif" },
+  { name: "Noto Sans", value: "Noto Sans, sans-serif" },
+  { name: "Droid Sans", value: "Droid Sans, sans-serif" },
+  { name: "Comic Sans MS", value: "Comic Sans MS, cursive, sans-serif" },
+  { name: "Impact", value: "Impact, sans-serif" },
+  { name: "Arial Black", value: "Arial Black, sans-serif" },
+  { name: "Copperplate", value: "Copperplate, serif" },
+  { name: "Brush Script MT", value: "Brush Script MT, cursive" },
+  { name: "Lucida Sans Unicode", value: "Lucida Sans Unicode, sans-serif" },
+  { name: "MS Sans Serif", value: "MS Sans Serif, sans-serif" },
+  { name: "MS Serif", value: "MS Serif, serif" },
+];
--- PM/src/entities/ui/components/select-font-menu/ui/select-font-menu.tsx
+++ PM/src/entities/ui/components/select-font-menu/ui/select-font-menu.tsx
@@ -1,13 +1,14 @@
 import style from "./select-font-menu.module.css";
+import { fonts } from "../model/fonts.ts";
 
 export const SelectFontMenu = () => {
   return (
     <div className={style.menu}>
       <div className={style.menu__wrapper}>
         <select className={style.menu__select}>
-          <option>Arial</option>
-          <option>Arial</option>
-          <option>Arial</option>
+          {fonts.map((font) => (
+            <option>{font.name}</option>
+          ))}
         </select>
       </div>
     </div>

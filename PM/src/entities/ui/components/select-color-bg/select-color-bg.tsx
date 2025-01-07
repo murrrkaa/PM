3c04a3024e1879dfcd5257136bd989670540b06b
Maria Babina <mashababina857@gmail.com>
2025-01-07 14:49:18 +0300

    added icon to change bg

--- PM/src/entities/ui/components/select-color-bg/select-color-bg.tsx
+++ PM/src/entities/ui/components/select-color-bg/select-color-bg.tsx
@@ -2,6 +2,7 @@ import style from "./select-color-bg.module.css";
 import { FC } from "react";
 import { TypeBackground } from "../../../../shared/ui/model/types";
 import { InputSelectColor } from "../../../../shared/ui/components/input-select-color/ui";
+import { Pipette } from "../../../../shared/ui/icons/pipette.tsx";
 
 interface IProp {
   onChange: (type: TypeBackground | string, color: string) => void;
@@ -11,7 +12,7 @@ export const SelectColorBg: FC<IProp> = ({ onChange }) => {
   return (
     <div className={style.select__color}>
       <span>Выбрать свой цвет</span>
-      <InputSelectColor onChange={onChange} />
+      <InputSelectColor onChange={onChange} icon={<Pipette />} />
     </div>
   );
 };
